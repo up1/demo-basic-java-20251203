c6d5dedd597e3fa3647182cb4a091b274231e8a2
up1 <somkiat.p@gmail.com>
2025-12-03 12:18:52 +0700

    Improve code

--- day01/src/main/java/org/example/Hello.java
+++ day01/src/main/java/org/example/Hello.java
@@ -3,7 +3,16 @@ package org.example;
 public class Hello {
 
     public String sayHi(String name){
-        if("xxx".equals(name)) {
+        String xxx = "xxx";
+
+        // Fail fast
+        if(name == null) {
+            return "Error";
+        }
+
+        // Success
+
+        if(name.equals(xxx)) {
             return "xxx";
         }
         return "Hello " + name;
