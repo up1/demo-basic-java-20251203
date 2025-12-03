43b30aa4c00afa50d6475582a904e303918d5683
up1 <somkiat.p@gmail.com>
2025-12-03 14:41:29 +0700

    Demo with java Record

--- day01/src/main/java/org/example/DemoObject.java
+++ day01/src/main/java/org/example/DemoObject.java
@@ -3,28 +3,14 @@ package org.example;
 
 import java.util.Objects;
 
-class User {
-    int id;
-    String name;
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return id == user.id && Objects.equals(name, user.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name);
-    }
+record User(int id, String name){
 }
 
 public class DemoObject {
 
     static void main(String[] args) {
-        User u1 = new User(); // Default constructor
-        User u2 = new User();
+        User u1 = new User(1, ""); // Default constructor
+        User u2 = new User(1, "");
         System.out.println(u1.equals(u2));
         System.out.println(u1.toString());
         System.out.println(u2.toString());
