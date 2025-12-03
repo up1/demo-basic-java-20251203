d49db01efa6a6364bc4826edbb8f9729a63b53ad
up1 <somkiat.p@gmail.com>
2025-12-03 10:44:15 +0700

    Start workshop

---
+++ day01/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    // Class variable
+    private String firstName;
+    private String firstname;
+    private String first_name;
+    private String fname;
+
+    static void main(String[] args) {
+        // Method variable or local variable
+        int i = 0;
+        System.out.println("Hello Java");
+    }
+}
