f23dd068f6f61311d709ae29cf21ec57ef3fac0f
up1 <somkiat.p@gmail.com>
2025-12-03 17:12:15 +0700

    Refactor by IDE => refactor -> extract to method

--- day01/src/main/java/org/example/DemoRange.java
+++ day01/src/main/java/org/example/DemoRange.java
@@ -3,19 +3,33 @@ package org.example;
 public class DemoRange {
 
     public String get(String input){
-        int start = Integer.parseInt(input.charAt(1) + "") + 1;
-        if(input.startsWith("[")) {
-            start = Integer.parseInt(input.charAt(1) + "");
+        int start = getStartNumber(input);
+        int end = getEndNumber(input);
+        return generateResult(start, end);
+    }
+
+    private static String generateResult(int start, int end) {
+        String result = "";
+        for (int i = start; i <= end; i++) {
+            result = result + i;
         }
+        return result;
+    }
+
+    private static int getEndNumber(String input) {
         int end = Integer.parseInt(input.charAt(3) + "") - 1;
         if(input.endsWith("]")) {
             end = Integer.parseInt(input.charAt(3) + "");
         }
-        String result = "";
-        for (int i = start; i <= end ; i++) {
-            result = result + i;
+        return end;
+    }
+
+    private static int getStartNumber(String input) {
+        int start = Integer.parseInt(input.charAt(1) + "") + 1;
+        if(input.startsWith("[")) {
+            start = Integer.parseInt(input.charAt(1) + "");
         }
-        return result;
+        return start;
     }
 
 }
