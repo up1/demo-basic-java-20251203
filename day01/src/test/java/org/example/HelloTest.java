da9bb7ff3c744b5893b89764839f75e9ee65d63b
up1 <somkiat.p@gmail.com>
2025-12-03 11:44:42 +0700

    Add test description

--- day01/src/test/java/org/example/HelloTest.java
+++ day01/src/test/java/org/example/HelloTest.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class HelloTest {
 
     @Test
+    @DisplayName("say hi with somkiat")
     void sayHi() {
         Hello hello = new Hello();
         String expected = "Hello somkiat";
