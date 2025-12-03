fdba011a58f18c8e1a587d1a9e5afb0fbba7e65b
up1 <somkiat.p@gmail.com>
2025-12-03 15:56:27 +0700

    Demo with string and array

---
+++ day01/src/main/java/org/example/DemoString.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class DemoString {
+    public static void main(String[] args) {
+        String fname = "Somkiat";
+
+        // Loop in java
+        // 1. for
+        String[] names = new String[]{"A", "B"};
+        for (int i = 0; i < names.length ; i++) {
+            // For i
+            // Array Index Out of bound -1
+        }
+
+        for (String name : names) {
+            // For each
+
+        }
+        // 2. do-while
+        // 3. while()
+        int j=0;
+        while (j<5) {
+            j++;
+        }
+
+        for(int a=0; a<5; a++) {
+
+        }
+    }
+}
