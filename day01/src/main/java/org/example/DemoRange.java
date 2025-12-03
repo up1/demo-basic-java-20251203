fe12f37ebf6195497943f7191d0636cb84d794f5
up1 <somkiat.p@gmail.com>
2025-12-03 16:47:38 +0700

    Refactor

--- day01/src/main/java/org/example/DemoRange.java
+++ day01/src/main/java/org/example/DemoRange.java
@@ -4,17 +4,17 @@ public class DemoRange {
 
     public String get(String input){
         int start = Integer.parseInt(input.charAt(1) + "");
-        int end = 0;
+        int end;
         if(input.endsWith("]")) {
             end = Integer.parseInt(input.charAt(3) + "");
         } else {
             end = Integer.parseInt(input.charAt(3) + "") - 1;
         }
-        String result = "";
+        StringBuilder result = new StringBuilder();
         for (int i = start; i <= end ; i++) {
-            result += i;
+            result.append(i);
         }
-        return result;
+        return result.toString();
     }
 
 }
