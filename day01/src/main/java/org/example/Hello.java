c8a9127ef25977cd599e4b93c1f578a5b07bfe94
up1 <somkiat.p@gmail.com>
2025-12-03 11:55:17 +0700

    Add more cases

--- day01/src/main/java/org/example/Hello.java
+++ day01/src/main/java/org/example/Hello.java
@@ -3,6 +3,9 @@ package org.example;
 public class Hello {
 
     public String sayHi(String name){
+        if("xxx".equals(name)) {
+            return "xxx";
+        }
         return "Hello " + name;
     }
 
--- day01/src/test/java/org/example/HelloTest.java
+++ day01/src/test/java/org/example/HelloTest.java
@@ -8,11 +8,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class HelloTest {
 
     @Test
-    @DisplayName("say hi with somkiat")
+    @DisplayName("say hi with somkiat => Hello somkiat")
     void sayHi() {
         Hello hello = new Hello();
         String expected = "Hello somkiat";
         String actual = hello.sayHi("somkiat");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("say hi with xxx => xxx")
+    void sayHi2() {
+        Hello hello = new Hello();
+        String expected = "xxx";
+        String actual = hello.sayHi("xxx");
         assertEquals(expected, actual);
     }
 }
