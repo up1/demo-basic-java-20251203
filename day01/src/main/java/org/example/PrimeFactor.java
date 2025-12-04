71a41d953c69eb1ee912abcf6518f42e545ab955
up1 <somkiat.p@gmail.com>
2025-12-04 11:44:03 +0700

    Pass case 02

--- day01/src/main/java/org/example/PrimeFactor.java
+++ day01/src/main/java/org/example/PrimeFactor.java
@@ -7,6 +7,11 @@ public class PrimeFactor {
 
     public List<Integer> find(int input) {
         List<Integer> result = new ArrayList<>();
+        int i = 2;
+        if(input > 2) {
+            result.add(i);
+            input = input/i;
+        }
         result.add(input);
         return result;
     }
