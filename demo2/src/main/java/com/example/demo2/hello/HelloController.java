5eef5c438837a791ded8ca2cbadb7b116ee2ebff
up1 <somkiat.p@gmail.com>
2025-12-04 16:50:38 +0700

    Config REST Controller

--- demo2/src/main/java/com/example/demo2/hello/HelloController.java
+++ demo2/src/main/java/com/example/demo2/hello/HelloController.java
@@ -1,7 +1,12 @@
 package com.example.demo2.hello;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
 
+    @GetMapping("/hello")
     public Message sayHi(){
         return new Message("Hello Spring Boot");
     }
