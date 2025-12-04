30a16938233e13b786389c2eac8558b933810a47
up1 <somkiat.p@gmail.com>
2025-12-04 16:45:40 +0700

    Create hello controller

---
+++ demo2/src/main/java/com/example/demo2/hello/HelloController.java
@@ -0,0 +1,9 @@
+package com.example.demo2.hello;
+
+public class HelloController {
+
+    public Message sayHi(){
+        return new Message("Hello Spring Boot");
+    }
+
+}
---
+++ demo2/src/main/java/com/example/demo2/hello/Message.java
@@ -0,0 +1,6 @@
+package com.example.demo2.hello;
+
+public record Message(
+	String message
+) {
+}
